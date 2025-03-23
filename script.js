3e47e34700e2697f05a51a5e3d003d6375f2c4a1
ATCHAYA2616 <k.s.atchaya26@gmail.com>
2025-03-23 10:24:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar Scroll Effect
+document.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
